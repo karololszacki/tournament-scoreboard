c15d0fb0ee1c168ddd37eb5b69d7e7f50750d343
ruranek <r.uranek@timecamp.com>
2019-01-03 09:53:41 +0100

    React app

---
+++ front-end/src/components/DashboardPage/component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function DashboardPage() {
+  return (
+    <div className="DashboardPage">
+
+    </div>
+  );
+}
+
+export default DashboardPage;
